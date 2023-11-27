10c9c44263e5989b9193f3fcf1c7c3ca9c7f265e
ZHUOY-LAPTOP\zhuoy <zhuoyue_cn@yeah.net>
2023-11-27 21:41:47 +0800

    两线段关系判断支持容差

--- extensions/glodon/spatial_relationship_of_two_lines.py
+++ extensions/glodon/spatial_relationship_of_two_lines.py
@@ -7,12 +7,13 @@ from typing import Optional, Union, Tuple, List
 import numpy as np
 from shapely import Point, MultiPoint
 from PySide6.QtCore import Qt, Slot, Signal, Property, QObject
-from PySide6.QtWidgets import QDialog, QWidget, QLineEdit, QLabel, QSlider, QHBoxLayout
+from PySide6.QtWidgets import QDialog, QWidget, QLineEdit, QLabel, QHBoxLayout, QSpacerItem, QSizePolicy
 from PySide6.QtGui import QCloseEvent
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from matplotlib.pyplot import Line2D
 
+from shared import math as m
 from command import ICommand
 from ui import WidgetModelBase, WidgetViewModelBase, DialogBase
 from ui.utils import create_row_title, create_row_layout, create_column_layout, add_layout_children
@@ -31,7 +32,9 @@ class Line(object):
     def __eq__(self, other):
         if not isinstance(other, Line):
             return False
-        return self.start_point == other.start_point and self.end_point == other.end_point
+        start_point_distance = self.start_point.distance(other.start_point)
+        end_point_distance = self.end_point.distance(other.end_point)
+        return m.is_zero(start_point_distance) and m.is_zero(end_point_distance)
 
 
 BBox = Tuple[float, float, float, float]
@@ -44,6 +47,14 @@ class SpatialRelationship(IntEnum):
     PERPENDICULAR = auto()
 
 
+def is_close(a: Union[float, None], b: Union[float, None], tol: Optional[float] = None) -> bool:
+    if a is None and b is None:
+        return True
+    if a is not None and b is not None:
+        return m.is_close(a, b, tol)
+    return False
+
+
 def calculate_angle(first_line: Line, second_line: Line) -> Tuple[bool, float, str]:
     """计算两条直线段之间的夹角
 
@@ -53,9 +64,11 @@ def calculate_angle(first_line: Line, second_line: Line) -> Tuple[bool, float, s
 
     Returns: 计算结果，三元组 [结果是否有效，角度，错误信息]
     """
-    if first_line.start_point.distance(first_line.end_point) == 0:
+    first_segment_length = first_line.start_point.distance(first_line.end_point)
+    if is_close(first_segment_length, 0):
         return False, 0, '第一条线段长度为 0'
-    if second_line.start_point.distance(second_line.end_point) == 0:
+    second_segment = second_line.start_point.distance(second_line.end_point)
+    if is_close(second_segment, 0):
         return False, 0, '第二条线段长度为 0'
     first_start_point = np.array([first_line.start_point.x, first_line.start_point.y])
     first_end_point = np.array([first_line.end_point.x, first_line.end_point.y])
@@ -65,15 +78,15 @@ def calculate_angle(first_line: Line, second_line: Line) -> Tuple[bool, float, s
     second_vector = second_start_point - second_end_point
     angle = math.atan2(np.linalg.det([first_vector, second_vector]), np.dot(first_vector, second_vector))
     angle = math.fabs(np.degrees(angle)) % 180
-    if angle > 90:
+    if m.greater_than(angle, 90):
         angle = 180 - angle
     return True, angle, ''
 
 
 def infer_spatial_relationship(angle: float, tolerance: float) -> SpatialRelationship:
-    if angle == 90:
+    if m.is_close(angle, 90, tolerance):
         return SpatialRelationship.PERPENDICULAR
-    elif angle == 0:
+    elif m.is_zero(angle, tolerance):
         return SpatialRelationship.PARALLEL
     else:
         return SpatialRelationship.UNKNOWN
@@ -84,7 +97,7 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
     first_line_changed = Signal(type(Union[Line, None]))
     second_line_changed = Signal(type(Union[Line, None]))
     angle_changed = Signal(type(Union[float, None]))
-    tolerance_changed = Signal(type(float))
+    tolerance_changed = Signal(type(Union[float, None]))
     conclusion_changed = Signal(type(SpatialRelationship))
 
     def __init__(self, parent: Optional[QObject] = None):
@@ -92,7 +105,7 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
         self.__first_line: Union[Line, None] = None
         self.__second_line: Union[Line, None] = None
         self.__angle: Union[float, None] = None
-        self.__tolerance: float = 0
+        self.__tolerance: Union[float, None] = None
         self.__conclusion: SpatialRelationship = SpatialRelationship.UNKNOWN
 
     def get_first_line(self) -> Union[Line, None]:
@@ -120,21 +133,21 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
         return self.__angle
 
     def set_angle(self, value: Union[float, None]):
-        if self.angle != value:
+        if not is_close(self.angle, value):
             self.__angle = value
             self.angle_changed.emit(self.angle)
 
     angle = Property(type(Union[float, None]), fget=get_angle, fset=set_angle, notify=angle_changed)
 
-    def get_tolerance(self) -> float:
+    def get_tolerance(self) -> Union[float, None]:
         return self.__tolerance
 
-    def set_tolerance(self, value: float):
-        if self.tolerance != value:
+    def set_tolerance(self, value: Union[float, None]):
+        if not is_close(self.tolerance, value):
             self.__tolerance = value
             self.tolerance_changed.emit(self.tolerance)
 
-    tolerance = Property(type(float), fget=get_tolerance, fset=set_tolerance, notify=tolerance_changed)
+    tolerance = Property(type(Union[float, None]), fget=get_tolerance, fset=set_tolerance, notify=tolerance_changed)
 
     def get_conclusion(self) -> SpatialRelationship:
         return self.__conclusion
@@ -148,12 +161,16 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
                           notify=conclusion_changed)
 
     def initialize(self):
+        # connect
         self.first_line_changed.connect(self.__on_first_line_changed)
         self.second_line_changed.connect(self.__on_second_line_changed)
         self.angle_changed.connect(self.__on_angle_changed)
+        self.tolerance_changed.connect(self.__on_tolerance_changed)
+        # update
+        self.update()
 
     def update(self):
-        pass
+        self.tolerance = np.degrees(m.FLOAT_TOLERANCE)
 
     def update_angle(self):
         if self.first_line is None or self.second_line is None:
@@ -161,6 +178,12 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
         else:
             ok, angle, error = calculate_angle(self.get_first_line(), self.get_second_line())
             self.angle = angle if ok else None
+
+    def update_conclusion(self):
+        if self.angle is None:
+            self.conclusion = SpatialRelationship.UNKNOWN
+        else:
+            self.conclusion = infer_spatial_relationship(self.angle, self.tolerance)
 
     @Slot(type(Union[Line, None]))
     def __on_first_line_changed(self, first_line: Union[Line, None]):
@@ -172,10 +195,11 @@ class SpatialRelationshipOfTwoLinesModel(WidgetModelBase):
 
     @Slot(type(Union[float, None]))
     def __on_angle_changed(self, angle: Union[float, None]):
-        if angle is None:
-            self.conclusion = SpatialRelationship.UNKNOWN
-        else:
-            self.conclusion = infer_spatial_relationship(angle, self.get_tolerance())
+        self.update_conclusion()
+
+    @Slot(type(Union[float, None]))
+    def __on_tolerance_changed(self, tolerance: Union[float, None]):
+        self.update_conclusion()
 
 
 class SpatialRelationshipOfTwoLinesViewModel(WidgetViewModelBase):
@@ -220,18 +244,18 @@ class SpatialRelationshipOfTwoLinesViewModel(WidgetViewModelBase):
         self.model.initialize()
 
     def update(self):
-        pass
+        self.model.update()
 
     @Slot(type(Union[float, None]))
     def __on_model_angle_changed(self, angle: Union[float, None]):
-        self.angle_text = str() if angle is None else f'{angle} degree'
+        self.angle_text = str() if angle is None else f'{angle} degree, {np.radians(angle)} radian'
 
     @Slot(type(SpatialRelationship))
     def __on_model_conclusion_changed(self, conclusion: SpatialRelationship):
         self.conclusion_text = {
             SpatialRelationship.UNKNOWN: '',
-            SpatialRelationship.PARALLEL: '平行',
-            SpatialRelationship.PERPENDICULAR: '垂直',
+            SpatialRelationship.PARALLEL: 'Parallel',
+            SpatialRelationship.PERPENDICULAR: 'Perpendicular',
         }[conclusion]
 
 
@@ -241,7 +265,7 @@ class DoubleInput(QLineEdit):
 
     def __init__(self, parent: Optional[QWidget] = None):
         super().__init__(parent)
-        self.textChanged.connect(self.__on_text_changed)
+        self.editingFinished.connect(self.__on_editing_finished)
         self.__value: Union[float, None] = None
 
     def get_value(self) -> Union[float, None]:
@@ -256,16 +280,24 @@ class DoubleInput(QLineEdit):
     value = Property(type(Union[float, None]), fget=get_value, fset=set_value, notify=value_changed)
 
     def update_view(self):
-        if self.value is None:
-            self.setText('')
+        current_value = self.get_value_from_input(self.text())
+        if not is_close(current_value, self.value, m.FLOAT_TOLERANCE):
+            if self.value is None:
+                self.setText('')
+            else:
+                self.setText(f'{self.value:.7f}')
 
-    @Slot(str)
-    def __on_text_changed(self, text: str):
+    @Slot()
+    def __on_editing_finished(self):
+        self.value = self.get_value_from_input(self.text())
+
+    @staticmethod
+    def get_value_from_input(text: str) -> Union[float, None]:
         try:
             value = float(text)
-            self.value = value
+            return value
         except ValueError:
-            self.value = None
+            return None
 
 
 def create_symbol_label(text: str) -> QLabel:
@@ -382,6 +414,8 @@ class Visualizer(FigureCanvasQTAgg):
             delta_x = box[2] - box[0]
             delta_y = box[3] - box[1]
             radius = max(delta_x, delta_y) / 2.0 * 1.1
+            if m.is_zero(radius):
+                radius = 1
 
             center_x = (box[0] + box[2]) / 2.0
             x_min_limit = center_x - radius
@@ -444,11 +478,23 @@ class SpatialRelationshipOfTwoLinesView(object):
         self.angle_title = create_row_title(r'Angle')
         self.angle = QLineEdit()
         self.angle.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.angle.setStyleSheet(
+            '''
+            font-family: 'Consolas';
+            font-size: 14px;
+            '''
+        )
         self.angle_layout = create_row_layout(title=self.angle_title, widget=self.angle)
 
         self.tolerance_title = create_row_title(r'Tolerance')
-        self.tolerance_slider = QSlider(Qt.Orientation.Horizontal)
-        self.tolerance_layout = create_row_layout(title=self.tolerance_title, widget=self.tolerance_slider)
+        self.degree_input, self.degree_unit = self.create_tolerance_input('degree')
+        self.tolerance_spacer = QSpacerItem(24, 0, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.radian_input, self.radian_unit = self.create_tolerance_input('radian')
+        self.tolerance_layout = create_row_layout(
+            title=self.tolerance_title,
+            widgets=[self.degree_input, self.degree_unit, self.tolerance_spacer, self.radian_input, self.radian_unit],
+            append_spacer=True
+        )
 
         self.conclusion_title = create_row_title(r'Conclusion')
         self.conclusion = QLineEdit()
@@ -467,6 +513,27 @@ class SpatialRelationshipOfTwoLinesView(object):
         )
         self.layout.setContentsMargins(8, 8, 8, 8)
         dialog.setLayout(self.layout)
+
+    @staticmethod
+    def create_tolerance_input(unit: str) -> Tuple[DoubleInput, QLabel]:
+        value_input = DoubleInput()
+        value_input.setFixedWidth(96)
+        value_input.setStyleSheet(
+            '''
+            font-family: 'Consolas';
+            font-size: 14px;
+            '''
+        )
+        unit_label = QLabel(unit)
+        unit_label.setFixedWidth(48)
+        unit_label.setStyleSheet(
+            '''
+            margin-left: 4px;
+            font-family: 'Consolas';
+            font-size: 13px;
+            '''
+        )
+        return value_input, unit_label
 
 
 class SpatialRelationshipOfTwoLinesDialog(DialogBase):
@@ -493,14 +560,25 @@ class SpatialRelationshipOfTwoLinesDialog(DialogBase):
         get_ui_cache().save()
 
     def initialize(self):
-        self.ui.first_line_input.line_changed.connect(self.__on_first_line_changed)
-        self.ui.second_line_input.line_changed.connect(self.__on_second_line_changed)
+        # signals from view
+        self.ui.first_line_input.line_changed.connect(self.__on_ui_first_line_changed)
+        self.ui.second_line_input.line_changed.connect(self.__on_ui_second_line_changed)
+        self.ui.degree_input.value_changed.connect(self.__on_ui_tolerance_degree_value_changed)
+        self.ui.radian_input.value_changed.connect(self.__on_ui_tolerance_radian_value_changed)
+
+        # signals from view model
         self.vm.angle_text_changed.connect(self.ui.angle.setText)
         self.vm.conclusion_text_changed.connect(self.ui.conclusion.setText)
+
+        # signals from model
+        self.vm.model.tolerance_changed.connect(self.__on_model_tolerance_degree_value_changed)
+        self.vm.model.tolerance_changed.connect(self.__on_model_tolerance_radian_value_changed)
+
+        # initialize
         self.vm.initialize()
 
     @Slot(type(Union[Line, None]))
-    def __on_first_line_changed(self, line: Union[Line, None]):
+    def __on_ui_first_line_changed(self, line: Union[Line, None]):
         self.vm.model.first_line = line
         if line is None:
             self.ui.visualizer.remove_first_line()
@@ -508,12 +586,31 @@ class SpatialRelationshipOfTwoLinesDialog(DialogBase):
             self.ui.visualizer.update_first_line(line)
 
     @Slot(type(Union[Line, None]))
-    def __on_second_line_changed(self, line: Union[Line, None]):
+    def __on_ui_second_line_changed(self, line: Union[Line, None]):
         self.vm.model.second_line = line
         if line is None:
             self.ui.visualizer.remove_second_line()
         else:
             self.ui.visualizer.update_second_line(line)
+
+    @Slot(type(Union[float, None]))
+    def __on_ui_tolerance_degree_value_changed(self, value: Union[float, None]):
+        self.vm.model.tolerance = value
+
+    @Slot(type(Union[float, None]))
+    def __on_ui_tolerance_radian_value_changed(self, value: Union[float, None]):
+        self.vm.model.tolerance = None if value is None else np.degrees(value)
+
+    @Slot(type(Union[float, None]))
+    def __on_model_tolerance_degree_value_changed(self, value: Union[float, None]):
+        self.ui.degree_input.set_value(value)
+
+    @Slot(type(Union[float, None]))
+    def __on_model_tolerance_radian_value_changed(self, value: Union[float, None]):
+        if value is None:
+            self.ui.radian_input.set_value(None)
+        else:
+            self.ui.radian_input.set_value(np.radians(value))
 
 
 class SpatialRelationshipOfTwoLinesCommand(ICommand):
